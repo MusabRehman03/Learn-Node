f72e0f71f7885688c93320a26e61444e30df4c3b
Musab Ur Rehman <rehmanmusab0302@gmail.com>
2025-09-08 20:13:04 +0500

    implemennted register functionality

--- post-project/app.js
+++ post-project/app.js
@@ -1,2 +1,34 @@
 //await user.save krna pryga jab manually kuch data add kernnygy db mein withoour model.builtInFunction() like model.findOneAndUpdate()
 //findOne({}).populate('posts') .... so that id ki jgh py data ajaey for temporary storage for usage
+
+const express = require('express')
+const app =  express()
+const path = require('path')
+const userModel = require('./models/user')
+const bcrypt = require('bcrypt')
+
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/',function(req, res){
+    res.render('register')
+})
+app.post('/register', async function (req, res) {
+    const {username, email, password, imageUrl} = req.body
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            userModel.create({
+                username,
+                email,
+                password: hash,
+                imageUrl
+            })
+        });
+        res.redirect('login')
+    });
+
+})
+
+app.listen(3000)

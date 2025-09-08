1c9988ed16e17b8ba491bdc7dadcbf6e9eb5e295
Musab Ur Rehman <rehmanmusab0302@gmail.com>
2025-09-08 21:01:07 +0500

    feat: protected route implemennted for profile page

--- post-project/app.js
+++ post-project/app.js
@@ -7,8 +7,11 @@ const path = require('path')
 const userModel = require('./models/user')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 
 app.set('view engine', 'ejs')
+
+app.use(cookieParser())
 app.use(express.static(path.join(__dirname,'public')))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -39,17 +42,43 @@ app.get('/login', async function(req, res){
 app.post('/login',async function(req, res){
     const {email, password} = req.body
     const user = await userModel.findOne({email})
+    console.log(user)
     if(user){
         bcrypt.compare(password, user.password, function(err, result) {
             if(result){
                 var token = jwt.sign({ _id: user._id }, 'secret');
                 res.cookie('token', token)
+                console.log(token)
+                res.redirect('/profile')
+            }else{
+                res.send('invalid password')
             }
         });
-        res.redirect('/profile')
     }else{
         res.send("email or password incorrect")
     }
 })
+
+app.get('/profile', isLoggedIn, async (req, res)=>{
+    res.render('profile')
+})
+
+app.get('/logout', function(req, res){
+    res.cookie('token', '');
+    res.redirect('/login')
+})
+
+
+
+
+function isLoggedIn(req, res, next){
+    if(!req.cookies.token || req.cookies.token == ""){
+        res.status(500).send("Unauthorized, please Login first")
+    }else{
+        var decoded = jwt.verify(req.cookies.token, 'secret');
+        console.log(decoded)
+        next()
+    }
+}
 
 app.listen(3000)
